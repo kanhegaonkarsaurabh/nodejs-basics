79f9be334ea51505b61c7a94fe102eba286f3d7a
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-02-08 16:53:10 -0800

    starter file

--- express-app/app.js
+++ express-app/app.js
@@ -1,3 +1,4 @@
+var http = require('http');									 // For fetching data via some external api
 var express = require('express');
 var path = require('path');
 var bodyParser = require("body-parser");     // Needed for parsing the body
@@ -28,11 +29,20 @@ app.get('/contact', function(request,response){
 	response.sendFile('contact.html',{root:path.join(__dirname,'./views')});
 });
 
+// To get the feedback from the user to 
 app.post('/contact', function(request, response) {
 	if (request.body) {
 		response.sendFile('thanks.html', {root:path.join(__dirname,'./views')})
 	}
 });
+
+
+// External api stuff 
+request('https://restcountries.eu/rest/v2/name/india', {json: true}, (err, req, res) => {
+	if (err) console.log('OOOPS!');
+	console.log(res.body);
+});
+
 
 app.listen(3000,function(){
 	console.log('Listening at port 3000...');
